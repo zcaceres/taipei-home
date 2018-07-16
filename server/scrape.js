168aa66546c6c9927d91441a667a1bfc2c8066bb
zc <zachcaceres@gmail.com>
2018-07-16 23:22:23 +0800

    fix: watermark

--- server/scrape.js
+++ server/scrape.js
@@ -10,7 +10,7 @@ function nodeListToArray(nodeList) {
 function bodyData(body) {
   try {
     const bodyDOM = jsdom(body)
-    const imageMeta = bodyDOM.window.document.head.querySelector('meta[property="og:image"]')
+    const imageMeta = bodyDOM.window.document.head.querySelector('meta[property="og:image"]').replace('.water3', '')
     return imageMeta ? imageMeta.content : ''
   } catch (e) {
     return null
